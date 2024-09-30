5b7add52f273d759206036e4aaf7f04c904c43dc
Jubilee Marshall <jam1703@nyu.edu>
2024-09-30 09:02:58 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 function diamond(x, y, c) {
   push();
   translate(x, y);
+  strokeWeight(6);
   line(0, -c, c, 0);
   line(c, 0, 0, c);
   line(0, c, -c, 0);
@@ -12,25 +13,33 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   background(149, 163, 179);
   strokeWeight(3);
-  stroke(75, 78, 109);
+  stroke(255, 255, 255);
   fill(75, 78, 109);
 
-    // draw a column of rows of diamonds
-    for (let y = 100; y < height; y += 200) {
-      for (let x = 100; x < width; x += 200) 
-        diamond(x, y, 100, 100);
-    }
+  // draw purple rectangles
+  for (let y = 25; y < height; y += 200) {
+    for (let x = 25; x < width; x += 200) rect(x, y, 150, 150);
+  }
+  // draw a column of rows of diamonds in green
+  stroke(132, 220, 198);
+  for (let y = 100; y < height + 100; y += 200) {
+    for (let x = 100; x < width + 100; x += 200) diamond(x, y, 100, 100);
+  }
 
-    push()
-    angleMode(DEGREES)
-    translate(0, 0)
-    rotate(-45)
-    rect(-200, 500, 150, 150)
-    pop()
-  
-
+  // draw a column of rows of tiny circles in green
+  stroke(132, 220, 198);
+  fill(132, 220, 198);
   for (let y = 100; y < height; y += 200) {
-    for (let x = 100; x < width; x += 200) 
-     rect(x, y, 100, 100)
+    for (let x = 100; x < width; x += 200) ellipse(x, y, 10, 10);
+  }
+
+  // draw a second column of rows of tiny circles in green
+  push();
+  translate(200, 200);
+  stroke(132, 220, 198);
+  fill(132, 220, 198);
+  for (let y = 0; y < height; y += 200) {
+    for (let x = 0; x < width; x += 200) ellipse(x, y, 10, 10);
+    pop();
   }
 }
